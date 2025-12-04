2b70507e32da730774af4dfb3a3dd51442cceec6
Jose Carrion <joseloc@gmail.com>
2025-12-04 16:57:25 -0500

    use zero down deployment

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
-set -xe
+set -e
 
-echo "Pulling latest image..."
-docker compose pull compritas
+echo "Pulling latest images..."
+docker compose pull compritas-blue compritas-green
 
-echo "Restarting application..."
-docker compose up -d compritas
+echo "Running migrations..."
+docker compose run --rm compritas-blue /app/bin/migrate
 
-# echo "Running migrations..."
-# docker compose run --rm compritas ./bin/migrate
+echo "Updating compritas-blue..."
+docker compose up -d --no-deps compritas-blue
+echo "Waiting for health check..."
+sleep 40
+
+echo "Updating compritas-green..."
+docker compose up -d --no-deps compritas-green
+echo "Waiting for health check..."
+sleep 40
 
 echo "Cleaning up old images..."
 docker image prune -f --filter "dangling=true"
